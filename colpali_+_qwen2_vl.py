b2809df3911414c8b1fb156f81977331619e9519
Adityahulk <58138716+Adityahulk@users.noreply.github.com>
2024-12-01 18:43:36 +0530

    reducing model from 7B to 2B

--- colpali_+_qwen2_vl.py
+++ colpali_+_qwen2_vl.py
@@ -322,7 +322,7 @@ def extract_pages_as_images(pdf_path, page_numbers, temp_image_dir):
 def prepare_vlm_input(image_paths, prompt_text):
     """Prepare inputs for the Vision-Language Model."""
     # Load Qwen2VL model and processor
-    model = Qwen2VLForConditionalGeneration.from_pretrained("Qwen/Qwen2-VL-7B-Instruct", device_map="auto")
+    model = Qwen2VLForConditionalGeneration.from_pretrained("Qwen/Qwen2-VL-2B-Instruct", device_map="auto")
     min_pixels = 256*28*28
     max_pixels = 1280*28*28
     processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
