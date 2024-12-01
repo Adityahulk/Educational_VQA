96a42269246e92d21367adc5f64b1c085fadf097
Adityahulk <58138716+Adityahulk@users.noreply.github.com>
2024-12-01 17:32:33 +0530

    commenting future code

--- colpali_+_qwen2_vl.py
+++ colpali_+_qwen2_vl.py
@@ -396,33 +396,33 @@ def process_query_across_pdfs(query, use_index_documents: bool):
     # Combine the results
     combined_output = "\n".join(output_texts)
     print(f"Combined Output for Query '{query}':\n{combined_output}")
-    ocr_texts = []
-    # # Process OCR on top results
-    for result in search_results:
-        doc_id = result["doc_id"]
-        print(doc_id)
-        pdf_path = doc_id_to_path[str(doc_id)]
-        print(pdf_path)
-        ocr_texts = process_pdf(pdf_path)
+    # ocr_texts = []
+    # # # Process OCR on top results
+    # for result in search_results:
+    #     doc_id = result["doc_id"]
+    #     print(doc_id)
+    #     pdf_path = doc_id_to_path[str(doc_id)]
+    #     print(pdf_path)
+    #     ocr_texts = process_pdf(pdf_path)
 
-    # # Combine texts from all documents
-    combined_text = "".join(ocr_texts)
-    print(combined_text)
+    # # # Combine texts from all documents
+    # combined_text = "".join(ocr_texts)
+    # print(combined_text)
 
-    # Prepare LLM and processor
-    model, processor, device = initialize_llm()
+    # # Prepare LLM and processor
+    # model, processor, device = initialize_llm()
 
-    # # Prepare input for the LLM
-    prompt = (
-        "You are asked to answer questions asked on a document image.\n"
-        "The answers to questions are short text spans taken verbatim from the document. "
-        "This means that the answers comprise a set of contiguous text tokens present in the document.\n\n"
-        f"Document: {combined_text} and the Question is: {query}, provide  the Answer:"
-    )
+    # # # Prepare input for the LLM
+    # prompt = (
+    #     "You are asked to answer questions asked on a document image.\n"
+    #     "The answers to questions are short text spans taken verbatim from the document. "
+    #     "This means that the answers comprise a set of contiguous text tokens present in the document.\n\n"
+    #     f"Document: {combined_text} and the Question is: {query}, provide  the Answer:"
+    # )
 
-    # # Generate output
-    output = generate_answer_with_llm(model, processor, prompt)
-    print("answer:- " + str(output))
+    # # # Generate output
+    # output = generate_answer_with_llm(model, processor, prompt)
+    # print("answer:- " + str(output))
     
 if __name__ == "__main__":
     process_query_across_pdfs("Explain Anodizing", False)
